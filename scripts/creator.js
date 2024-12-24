1b0c4c6334e29a14bb1b8c087d5ceeb87b554ba1
Jake Waldner <jc264843@dal.ca>
2024-12-24 15:07:42 -0400

    updated creator.js

--- scripts/creator.js
+++ scripts/creator.js
@@ -65,7 +65,7 @@ saveButton.addEventListener('click', () => {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'CustomConnections.csv';
+    a.download = 'ConnectionsLabGame.csv';
     a.click();
     URL.revokeObjectURL(url);
 });
